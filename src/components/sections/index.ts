7e2ac6173ecf66f8bf02b614dc188204628bc330
Jay Patidar <patidar-jay@users.noreply.github.com>
2026-07-29 12:27:50 +0530

    chore: remove all unused files, empty folders, and dead code exports

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,6 +1,6 @@
 export { default as HeroSection } from './HeroSection';
 export { default as ProductSection } from './ProductSection';
-export { default as ServicesSection } from './ServicesSection';
+
 export { default as WhyChooseUsSection } from './WhyChooseUsSection';
 export { default as CTASection } from './CTASection';
 export { default as ContactSection } from './ContactSection';
